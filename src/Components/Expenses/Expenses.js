c43e9814d238e278057fd8e652c34d3f23febe25
Akshithaa5 <jessierowe143@gmail.com>
2024-02-23 23:03:37 +0530

    Conditional Rendering

--- src/Components/Expenses/Expenses.js
+++ src/Components/Expenses/Expenses.js
@@ -18,7 +18,19 @@ const Expenses = (props) => {
 
   let expensesContent = <p className="no-expenses">No expenses found.</p>;
 
-  if (filteredExpenses.length > 0) {
+  if (filteredExpenses.length === 1) {
+    expensesContent = (
+      <div>
+        <ExpenseItems
+          key={filteredExpenses[0].id}
+          title={filteredExpenses[0].title}
+          amount={filteredExpenses[0].amount}
+          date={filteredExpenses[0].date}
+        />
+        <p className="single-expense-message">Only single expense here. Please add more...</p>
+      </div>
+    );
+  } else if (filteredExpenses.length > 1) {
     expensesContent = filteredExpenses.map((expense) => (
       <ExpenseItems
         key={expense.id}
@@ -41,5 +53,4 @@ const Expenses = (props) => {
     </div>
   );
 };
-
 export default Expenses;
