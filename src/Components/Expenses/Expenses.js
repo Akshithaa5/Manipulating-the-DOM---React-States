52308a98c560c4d256d07fdd89765a12bcfb7cf4
Akshithaa5 <jessierowe143@gmail.com>
2024-02-23 22:52:36 +0530

    Conditional Rendering

--- src/Components/Expenses/Expenses.js
+++ src/Components/Expenses/Expenses.js
@@ -16,7 +16,7 @@ const Expenses = (props) => {
     return expense.date.getFullYear().toString() === filteredYear;
   });
 
-  let expensesContent = <p>No expenses found.</p>;
+  let expensesContent = <p className="no-expenses">No expenses found.</p>;
 
   if (filteredExpenses.length > 0) {
     expensesContent = filteredExpenses.map((expense) => (
